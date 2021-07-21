e21fe2712e6d9d9ec7ba2d50f852a3dd90f3ad1a
NickieChandarana <84259467+NickieChandarana@users.noreply.github.com>
2021-07-21 18:52:16 +0530

    Add files via upload

---
+++ nickie.js
@@ -0,0 +1,15 @@
+function bed(){
+    window.location="bedroom.html"
+}
+function tv(){
+    window.location="tv.html"
+}
+function desk(){
+    window.location="desk.html"
+}
+function fruit(){
+    window.location="fruitbasket.html"
+}
+function bottles(){
+    window.location="bottles.html"
+}
